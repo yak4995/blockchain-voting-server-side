8c9a510a3cd4de54fef6a8806dd3bd9ccc58f305
Yurii Kryvokhyzha <yak4995@gmail.com>
2019-03-04 17:31:25 +0200

    Preparated for Heroku 2

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AllExceptionsFilter } from './common/all-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
