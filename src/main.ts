48bddb5cc41369ad90bf7665ea1d3446a8061724
Yurii Kryvokhyzha <yak4995@gmail.com>
2019-03-06 15:23:22 +0200

    README has been updated and added schema to swagger

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('BCVS')
     .addBearerAuth('jwt', 'header')
+    .setSchemes('http', 'https')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
